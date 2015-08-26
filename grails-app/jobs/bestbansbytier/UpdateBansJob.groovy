f1ecbe5b267a63f96593f33fecdfc2976743f32c
Jonathan Lutz <Jlutz@triu.com>
2015-08-26 16:05:31 -0400

    disable startup calculate bans job

--- grails-app/jobs/bestbansbytier/UpdateBansJob.groovy
+++ grails-app/jobs/bestbansbytier/UpdateBansJob.groovy
@@ -6,7 +6,7 @@ import jline.internal.Log
 class UpdateBansJob {
     BanCalculatorService banCalculatorService
     static triggers = {
-        simple name:'startUpTrigger', startDelay:10000, repeatInterval: 30000, repeatCount: 0
+        //simple name:'startUpTrigger', startDelay:10000, repeatInterval: 30000, repeatCount: 0
         cron name: 'dailyTrigger', cronExpression: "0 0 5 * * ?"
     }
 
