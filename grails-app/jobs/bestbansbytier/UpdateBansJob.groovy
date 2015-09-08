6a813eb17e45a199897667987ee0822fac379673
Jonathan Lutz <Jlutz@triu.com>
2015-09-07 23:39:04 -0400

    limit 5 try on job

--- grails-app/jobs/bestbansbytier/UpdateBansJob.groovy
+++ grails-app/jobs/bestbansbytier/UpdateBansJob.groovy
@@ -12,9 +12,42 @@ class UpdateBansJob {
 
     def execute() {
         System.out.println("Running ban calculation job")
+        List<ServerRegions> failedRegions = []
         ServerRegions.each { region ->
-            banCalculatorService.calculateData(region) //na, euw, eune, br
-            banCalculatorService.assignRanks(region)
+            try {
+                banCalculatorService.calculateData(region)
+                banCalculatorService.assignRanks(region)
+            }
+            catch(e) {
+                failedRegions.add(region)
+            }
+        }
+
+        def currentRegion = null
+        def currentRegionAttempts = 0
+        while(failedRegions) {
+            if(failedRegions[0] != currentRegion) {
+                currentRegion = failedRegions[0]
+                currentRegionAttempts = 0
+            }
+            else {
+                Thread.sleep(2000) // Sleep each tier just for safety on parsing win rates from op.gg
+                currentRegionAttempts += 1
+                if(currentRegionAttempts >= 5) {
+                    System.out.println("Unable to fetch data for ${currentRegion} during FINAL ATTEMPT")
+                    log.error("Unable to fetch data for ${currentRegion} during FINAL ATTEMPT")
+                    failedRegions.remove(0)
+                }
+            }
+            try {
+                banCalculatorService.calculateData(currentRegion)
+                banCalculatorService.assignRanks(currentRegion)
+                failedRegions.remove(0)
+            }
+            catch(e){
+                System.out.println("Failed regions ${currentRegion} again")
+                log.error("Failed regions ${currentRegion} again")
+            }
         }
     }
 }
